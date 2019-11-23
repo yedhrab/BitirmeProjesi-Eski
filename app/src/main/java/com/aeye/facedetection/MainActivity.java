c80acd6dfa5256285902e5260cc7e28aa9e6dd24
Yunus Emre <49655146+yedhrab@users.noreply.github.com>
2019-11-23 19:01:52 +0300

    🚧 Temel servis yapısı kuruldu

--- app/src/main/java/com/aeye/facedetection/MainActivity.java
+++ app/src/main/java/com/aeye/facedetection/MainActivity.java
@@ -1,14 +1,21 @@
 package com.aeye.facedetection;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn_gallery;
+    Button btn_gallery, btn_telemetry;
+
+    static final int TELEMETRY_CHANNEL_ID = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +28,11 @@ public class MainActivity extends AppCompatActivity {
 
     void initViews() {
         btn_gallery = findViewById(R.id.btn_gallery);
+        btn_telemetry = findViewById(R.id.btn_telemetry);
     }
 
     void bindEvents() {
-        btn_gallery.setOnClickListener((view) -> startActivity(new Intent(MainActivity.this, GalleryActivity.class)));
+        btn_gallery.setOnClickListener((view) -> startActivity(new Intent(this, GalleryActivity.class)));
+        btn_telemetry.setOnClickListener((view) -> startService(new Intent(this, TelemetryService.class)));
     }
-
 }
---
+++ app/src/main/java/com/aeye/facedetection/TelemetryService.java
@@ -0,0 +1,88 @@
+package com.aeye.facedetection;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.IBinder;
+import android.os.Looper;
+import android.os.Message;
+import android.os.Process;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
+
+public class TelemetryService extends Service {
+    private static final String TAG = "TelemetryService";
+
+    private Looper telemetryLooper;
+    private TelemetryHandler telemetryHandler;
+
+    public TelemetryService() {
+    }
+
+    private final class TelemetryHandler extends Handler {
+        public TelemetryHandler(Looper looper) {
+            super(looper);
+        }
+
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            try {
+                Log.i(TAG, "Mesaj alındı");
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                // Kesme isteği geldiğinde isteği uygulama
+                Thread.currentThread().interrupt();
+            }
+            stopSelf(msg.arg1);
+        }
+    }
+
+    @Override
+    public void onCreate() {
+        Log.i(TAG, "Servis oluşturuldu");
+
+        // Arkaplanda çalışacak thread'in tanımlanması ve başlatılması (UI thread'i bloklamaması lazım)
+        HandlerThread thread = new HandlerThread("TelemetryStartArguments", Process.THREAD_PRIORITY_BACKGROUND);
+        thread.start();
+
+        telemetryLooper = thread.getLooper();
+        telemetryHandler = new TelemetryHandler(telemetryLooper);
+    }
+
+
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i(TAG, "Servis başlatıldı");
+
+        Message msg = telemetryHandler.obtainMessage();
+        msg.arg1 = startId; // İsteklerin yönetimi için kimlikleri saklamalıyız
+        telemetryHandler.sendMessage(msg);
+
+        // Eğer servis öldüyse, bu dönüşten sonra Intent'siz tekrar başlat
+        return START_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+        // TODO: Return the communication channel to the service.
+        // throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.i(TAG, "Servis kapatıldı");
+    }
+}
