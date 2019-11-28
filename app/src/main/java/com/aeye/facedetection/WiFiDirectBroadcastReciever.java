eaa54d482e2fef5bf2e09538a80a9ce61a772a5e
Yunus Emre <49655146+yedhrab@users.noreply.github.com>
2019-11-28 21:25:22 +0300

    📃 Log'lar artırıldı, açıklamalar eklendi

--- app/src/main/java/com/aeye/facedetection/WiFiDirectBroadcastReciever.java
+++ app/src/main/java/com/aeye/facedetection/WiFiDirectBroadcastReciever.java
@@ -3,6 +3,8 @@ package com.aeye.facedetection;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.wifi.p2p.WifiP2pConfig;
+import android.net.wifi.p2p.WifiP2pDevice;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.net.wifi.p2p.WifiP2pManager.Channel;
 import android.util.Log;
@@ -35,38 +37,41 @@ public class WiFiDirectBroadcastReciever extends BroadcastReceiver {
         // TODO
         if (action != null) {
             switch (action) {
-                // Check to see if Wi-Fi is enabled and notify appropriate activity
+                // Wi-Fi P2P'nin aktif olup olmadığını anlama
                 case WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION:
-                    Log.d(TAG, "WIFI_P2P_STATE_CHANGED_ACTION");
+                    Log.d(TAG, "onReceive: Wi-Fi P2P dumur değişti");
 
-                    // WiFi P2P durumununa göre işlemler yapma
                     int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
                     if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
-                        Log.d(TAG, "WIFI_P2P_STATE_ENABLED");
+                        Log.d(TAG, "onReceive: Wi-Fi P2P aktif");
 
                     } else {
-                        Log.d(TAG, "WIFI_P2P_STATE_DISABLED");
+                        Log.d(TAG, "onReceive: Wi-Fi P2P aktif değil");
                     }
                     break;
                 // Call WifiP2pManager.requestPeers() to get a list of current peers
                 case WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION:
-                    Log.d(TAG, "WIFI_P2P_PEERS_CHANGED_ACTION");
+                    Log.d(TAG, "onReceive: Wi-Fi P2P Eşlenebilir cihazların listesi değişti");
 
                     if (manager != null) {
                         Log.d(TAG, "Request peers");
-                        // manager.requestPeers(channel, myPeerListListener);
+                        manager.requestPeers(channel, peers -> {
+                            Log.d(TAG, "onReceive: " + peers.getDeviceList());
+                            WifiP2pDevice device;
+                            WifiP2pConfig config = new WifiP2pConfig();
+                            // config.deviceAddress = device.deviceAddress;
+                        });
                     }
                     break;
                 // Respond to new connection or disconnections
                 case WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION:
-                    Log.d(TAG, "WIFI_P2P_CONNECTION_CHANGED_ACTION");
+                    Log.d(TAG, "onReceive: WiFi P2P bağlantısının durumu değişti");
                     break;
                 // Respond to this device's wifi state changing
                 case WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION:
-                    Log.d(TAG, "WIFI_P2P_THIS_DEVICE_CHANGED_ACTION");
+                    Log.d(TAG, "onReceive: Wi-Fi P2P cihaz eylemleri değişti");
                     break;
             }
         }
-
     }
 }
