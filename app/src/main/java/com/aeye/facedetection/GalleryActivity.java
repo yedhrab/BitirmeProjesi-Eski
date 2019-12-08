c5eae197268624391afb1fd014aec7c9ad1de4a6
Yunus Emre <49655146+yedhrab@users.noreply.github.com>
2019-12-08 17:18:12 +0300

    👨‍🔧 Listener yapısına göre düzeltildi

--- app/src/main/java/com/aeye/facedetection/GalleryActivity.java
+++ app/src/main/java/com/aeye/facedetection/GalleryActivity.java
@@ -8,68 +8,65 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.media.FaceDetector;
-import android.media.Image;
+import android.graphics.Rect;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.face.FirebaseVisionFace;
 
 import java.io.IOException;
-import java.net.URI;
 
 public class GalleryActivity extends AppCompatActivity {
 
-    private  static  final int IMAGE_PICK_CODE=1000;
-    private  static  final int PERMISSION_CODE=1001;
+    private static final int IMAGE_PICK_CODE = 1000;
+    private static final int PERMISSION_CODE = 1001;
 
     ImageView iv_detected_image;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
 
-        iv_detected_image=findViewById(R.id.iv_detected_image);
+        iv_detected_image = findViewById(R.id.iv_detected_image);
         Button btnChoseImage = findViewById(R.id.btnChoseImage);
 
 
-        btnChoseImage.setOnClickListener((view)  -> {
-                if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
-                    if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)== PackageManager.PERMISSION_DENIED){
-                      //permission no
-                        String[] permissions={Manifest.permission.READ_EXTERNAL_STORAGE};
-                        requestPermissions(permissions,PERMISSION_CODE);
-                    }
-                    else {
-                        //permission already granted
-                        PickImageFromGallery();
-                    }
-
-                }
-                else{
+        btnChoseImage.setOnClickListener((view) -> {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                    //permission no
+                    String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
+                    requestPermissions(permissions, PERMISSION_CODE);
+                } else {
+                    //permission already granted
                     PickImageFromGallery();
                 }
-            });
+
+            } else {
+                PickImageFromGallery();
+            }
+        });
     }
 
     private void PickImageFromGallery() {
-        Intent intent=new Intent(Intent.ACTION_PICK);
+        Intent intent = new Intent(Intent.ACTION_PICK);
         intent.setType("image/*");
-        startActivityForResult(intent,IMAGE_PICK_CODE);
+        startActivityForResult(intent, IMAGE_PICK_CODE);
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode){
-            case PERMISSION_CODE:{
-                if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+        switch (requestCode) {
+            case PERMISSION_CODE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     PickImageFromGallery();
-                }
-                else {
+                } else {
                     //
                 }
             }
@@ -78,22 +75,22 @@ public class GalleryActivity extends AppCompatActivity {
 
     @SuppressLint("MissingSuperCall")
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if (resultCode == RESULT_OK && requestCode == IMAGE_PICK_CODE) {
+        if (resultCode == RESULT_OK && requestCode == IMAGE_PICK_CODE && data != null) {
             Uri imageUri = data.getData();
             iv_detected_image.setImageURI(imageUri);
 
-            FaceDetection model = new FaceDetection();
-
             try {
-                FirebaseVisionImage fImage = model.converFirebaseVisionImage(this, imageUri);
-                model.detectFaces(fImage);
+                FirebaseVisionImage fImage = FaceDetectionAPI.converFirebaseVisionImage(this, imageUri);
+                FaceDetectionAPI.detectFaces(fImage, faceList -> {
+                    for (FirebaseVisionFace face : faceList) {
+                        // TODO: Burada işlemler yapılacak
+                        Rect bounds = face.getBoundingBox();
+                        Log.i(TAG, String.format("(%s, %s, %s, %s", bounds.bottom, bounds.left, bounds.right, bounds.top));
+                    }
+                });
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
-
-            //YUNUS EMRE - RESİMİN URİ SİNNİ BURDA ALDIK SEN BURDAN AL -MLKİT SOK
-
         }
     }
 
